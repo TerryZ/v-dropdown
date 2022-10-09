0261b6b1684243db556e7d9cffa311faa2f41f82
Terry Zeng <terry5@foxmail.com>
2022-10-09 20:49:53 +0800

    test: add test case template

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,18 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+
+import { Dropdown } from '@/'
+
+describe('v-dropdown', () => {
+  describe('v-dropdown', () => {
+    const wrapper = mount(Dropdown, {
+      props: {
+
+      }
+    })
+
+    it('', () => {
+      expect(wrapper).exist()
+    })
+  })
+})
