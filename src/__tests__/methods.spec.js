d3b5934c63efef8a15ad0c6a1683946c4fb63a68
TerryZ <terry5@foxmail.com>
2023-02-08 17:46:11 +0800

    test: 完善 methods 单元测试内容

--- src/__tests__/methods.spec.js
+++ src/__tests__/methods.spec.js
@@ -3,7 +3,6 @@ import { mount } from '@vue/test-utils'
 import { h, nextTick } from 'vue'
 
 import { Dropdown } from '@/'
-import { getCssDisplay } from './util'
 
 describe('v-dropdown methods', () => {
   const wrapper = mount(Dropdown, {
@@ -16,21 +15,21 @@ describe('v-dropdown methods', () => {
   it('call `display` method, the dropdown container should be display', async () => {
     wrapper.vm.display()
     await nextTick()
-    expect(getCssDisplay(wrapper.vm.container)).not.equal('none')
+    expect(wrapper.vm.visible).equal(true)
   })
   it('call `close` method, the dropdown container should be close', async () => {
     wrapper.vm.close()
     await nextTick()
-    expect(getCssDisplay(wrapper.vm.container)).equal('none')
+    expect(wrapper.vm.visible).equal(false)
   })
   it('call `toggleVisible` method, the dropdown container should be display', async () => {
     wrapper.vm.toggleVisible()
     await nextTick()
-    expect(getCssDisplay(wrapper.vm.container)).not.equal('none')
+    expect(wrapper.vm.visible).equal(true)
   })
   it('call `toggleVisible` method again, the dropdown container should be close', async () => {
     wrapper.vm.toggleVisible()
     await nextTick()
-    expect(getCssDisplay(wrapper.vm.container)).equal('none')
+    expect(wrapper.vm.visible).equal(false)
   })
 })
