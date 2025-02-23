59ff508019d474cc1ec3e4db0e2985e78dc960b3
Terry Zeng <terry5@foxmail.com>
2025-02-23 21:56:45 +0800

    feat: 设置下拉栏最大宽度，防止内容超出屏幕

--- examples/ExamplesIndex.vue
+++ examples/ExamplesIndex.vue
@@ -154,18 +154,7 @@ const onClosed = () => {
         </template>
         <DropdownContent :rounded="containerRounded">
           <div class="p-3">
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
-            <div>0123456789012345678901234567890123456789</div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
         </DropdownContent>
       </Dropdown>
